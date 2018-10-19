97b975e3853c5b2708f795476f15b77ef7ca47d6
Thiago Hirata <hirata@devcase.com.br>
2018-10-18 22:32:15 -0300

    mudança no nome do bundle gerado pelo i18n

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,8 @@ module.exports = [
             ]
         },
         output: {
-            filename: '[name].bundle.js',
-            path: path.resolve(__dirname, 'dist', locale)
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist', 'static', 'js', locale)
         },
         mode: "development"
     })),
